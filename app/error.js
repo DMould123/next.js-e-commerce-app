f59d19393755f124b5d2916098d28031bd79e101
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-09-14 00:31:14 +0200

    Added tutor pics and mapped through data from Stripe

---
+++ app/error.js
@@ -0,0 +1,11 @@
+'use client'
+export default function Error() {
+  return (
+    <div>
+      <img
+        src="https://media.giphy.com/media/mq5y2jHRCAqMo/giphy.gif"
+        alt="Error"
+      />
+    </div>
+  )
+}
---
+++ app/loading.js
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div>
+      <img
+        src="https://media.tenor.com/XasjKGMk_wAAAAAC/load-loading.gif"
+        alt="Loading..."
+      />
+    </div>
+  )
+}
