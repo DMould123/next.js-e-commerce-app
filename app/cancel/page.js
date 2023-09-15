cac42831294307833851bceff995d3a5ee699fef
DMould123 <95115610+DMould123@users.noreply.github.com>
2023-09-15 19:39:09 +0200

    added gifs to the cancel and success pages

--- app/cancel/page.js
+++ app/cancel/page.js
@@ -2,9 +2,17 @@ import Link from 'next/link'
 
 export default function CancelPage() {
   return (
-    <div>
-      Gutted
-      <Link href={'/'}>Back home</Link>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-semibold mb-4">Payment Cancelled</h1>
+      <img
+        src="https://media.giphy.com/media/xT5LMYlZh1HfoOtS6Y/giphy.gif"
+        alt="Cancelled GIF"
+        className="w-96 h-96 mb-4" // Adjust the width and height here
+      />
+      <p className="text-lg mb-4">Oops! Your payment was cancelled.</p>
+      <Link href={'/'}>
+        <p className="text-blue-500 hover:underline">Back to Home</p>
+      </Link>
     </div>
   )
 }
--- app/success/page.js
+++ app/success/page.js
@@ -2,9 +2,19 @@ import Link from 'next/link'
 
 export default function SuccessPage() {
   return (
-    <div>
-      Nice!
-      <Link href={'/'}>Back home</Link>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-semibold mb-4">Payment Successful</h1>
+      <img
+        src="https://media.giphy.com/media/Od0QRnzwRBYmDU3eEO/giphy.gif"
+        alt="Success GIF"
+        className="w-96 h-96 mb-4"
+      />
+      <p className="text-lg mb-4">
+        Congratulations! Your payment was successful.
+      </p>
+      <Link href={'/'}>
+        <p className="text-blue-500 hover:underline">Back to Home</p>
+      </Link>
     </div>
   )
 }
